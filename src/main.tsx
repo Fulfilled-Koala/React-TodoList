632a3737b45a8700e16d0e84120261a223644147
Fulfilled-Koala <fulfilled.koala@gmail.com>
2022-08-27 17:12:35 -0500

    feat: wrap application with theme provider and layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import ThemeProvider from "./context/ThemeProvider";
+import Layout from "./layouts/Layout";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <Layout>
+        <App />
+      </Layout>
+    </ThemeProvider>
   </React.StrictMode>
-)
+);
