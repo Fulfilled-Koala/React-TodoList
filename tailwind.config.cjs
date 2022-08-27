031e9fbd629fe90a81d38907455c678c926c181f
Fulfilled-Koala <fulfilled.koala@gmail.com>
2022-08-27 17:11:28 -0500

    feat: add custom font and custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,32 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import("tailwindcss").Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "sans": ["Lato", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        dim: {
+          50: "#383838",
+          100: "#363636",
+          200: "#333333",
+          300: "#2E2E2E",
+          400: "#2C2C2C",
+          500: "#272727",
+          600: "#242424",
+          700: "#222222",
+          800: "#1F1F1F",
+          900: "#1E1E1E",
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")({ strategy: "base" })],
 };
