d1f719ff42f7f726d9b80d35891d41afadc878e5
Fulfilled-Koala <fulfilled.koala@gmail.com>
2022-08-27 17:19:33 -0500

    feat: add input modal and todos list

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,58 @@
-import create from "zustand";
-
-type State = {
-  count: number;
-  increment: () => void;
-};
-
-const useStore = create<State>()((setState) => ({
-  count: 0,
-  increment: () => setState((state) => ({ count: state.count + 1 })),
-}));
+import { Tab } from "@headlessui/react";
+import { useState } from "react";
+import TodoTab from "./components/TodoTab";
+import Todo from "./components/Todo";
+import useTodos from "./store/useTodos";
+import FormModal from "./components/FormModal";
 
 export default function App(): JSX.Element {
-  const { count, increment } = useStore((state) => state);
+  const [showModal, setShowModal] = useState(false);
+  const todos = useTodos((state) => state.todos);
+
+  const activeTodos = todos.filter((todo) => !todo.completed);
+  const completedTodos = todos.filter((todo) => todo.completed);
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-4">
-      <button
-        type="button"
-        onClick={() => increment()}
-        className="inline-block rounded bg-blue-600 px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
-      >
-        Increment
-      </button>
-      <p className="text-lg">
-        Count is <span className="font-bold text-blue-700">{count}</span>
-      </p>
-    </div>
+    <>
+      <FormModal show={showModal} onClose={() => setShowModal(false)} />
+
+      <div className="flex h-1/2 w-auto flex-col justify-between rounded-md bg-white px-2 py-4 text-black shadow-md dark:bg-dim-300 dark:text-neutral-100 sm:px-6 xl:w-1/3">
+        <Tab.Group>
+          <Tab.List as="div" className="flex justify-between">
+            <TodoTab title="All Todos" amount={todos.length} />
+            <TodoTab title="Active Todos" amount={activeTodos.length} />
+            <TodoTab title="Completed Todos" amount={completedTodos.length} />
+          </Tab.List>
+
+          <Tab.Panels as="ul" className="my-4 h-full overflow-y-auto">
+            <Tab.Panel>
+              {todos.map((todo) => (
+                <Todo key={todo.id} todo={todo} />
+              ))}
+            </Tab.Panel>
+
+            <Tab.Panel>
+              {activeTodos.map((todo) => (
+                <Todo key={todo.id} todo={todo} />
+              ))}
+            </Tab.Panel>
+
+            <Tab.Panel>
+              {completedTodos.map((todo) => (
+                <Todo key={todo.id} todo={todo} />
+              ))}
+            </Tab.Panel>
+          </Tab.Panels>
+        </Tab.Group>
+
+        <button
+          onClick={() => setShowModal(true)}
+          type="button"
+          className="rounded-md bg-green-500 px-4 py-2 text-sm font-bold font-medium text-white shadow-xl duration-300 hover:bg-opacity-50 dark:bg-green-600 dark:hover:bg-opacity-50"
+        >
+          Create Todo
+        </button>
+      </div>
+    </>
   );
 }
